6eb76708037fca0a0c57dbb753a1c8bb4624349c
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2020-01-08 20:30:18 +0100

    Add basic routing events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+
+	goToStore = event => {
+		event.preventDefault();
+		const store = this.myInput.current.value;
+		this.props.history.push(`/store/${store}`);
+	};
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please, Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" />
+				<input type="text" required placeholder="Store Name" ref={this.myInput} defaultValue={getFunName()} />
 				<button type="submit">Visit Store →</button>
 			</form>
 		);
